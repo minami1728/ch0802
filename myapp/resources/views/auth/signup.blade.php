398a5b7778deb3db99fe9d346946248f1d22758c
“minami1728“ <ck6u35p@gmail.com>
2024-08-02 14:44:38 +0800

    在view signup內增設include

--- myapp/resources/views/auth/signup.blade.php
+++ myapp/resources/views/auth/signup.blade.php
@@ -7,12 +7,9 @@
 <!-- 傳送資料到母模板，並指定變數為 content -->
 @section('content') 
 
-<h1>{{ $title }}</h1> 
+<!-- 載入元件模板 components.socialButtons --> 
+@include('components.socialButtons') 
 
-<div class="social"> 
-    <a href="#">分享到 Facebook</a> 
-    <a href="#">分享到 Twitter</a> 
-</div> 
 Email： <input type="text" name="email" placeholder="Email" > 
 密碼： <input type="password" name="password" placeholder="密碼" > 
 暱稱： <input type="text" name="nickname" placeholder="暱稱" > 
